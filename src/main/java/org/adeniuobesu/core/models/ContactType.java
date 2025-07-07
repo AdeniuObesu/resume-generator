fefe97058ea58a5fb550808cd2c760bf254e5ae5
Anass Moukhafi <moukhafi.anass@gmail.com>
2025-07-07 07:03:53 +0100

    refactor: Simplify contact method types

--- src/main/java/org/adeniuobesu/core/models/ContactType.java
+++ src/main/java/org/adeniuobesu/core/models/ContactType.java
@@ -3,27 +3,9 @@ package org.adeniuobesu.core.models;
 public enum ContactType {
     EMAIL,
     PHONE,
-    MOBILE,
     LINKEDIN,
     GITHUB,
     PORTFOLIO,
-    WEBSITE,
-    TWITTER,
-    MASTODON,
-    DEV_TO,
-    STACK_OVERFLOW,
-    LEETCODE,
-    HACKER_RANK,
-    BLOG,
-    MEDIUM,
-    YOUTUBE,
-    DISCORD,
-    TELEGRAM,
-    SLACK,
-    WHATSAPP,
-    SIGNAL,
     CITY,
-    COUNTRY,
-    TIMEZONE,
-    PUBLIC_KEY
+    COUNTRY
 }
